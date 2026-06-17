0f4a378e6ef79024db2cadfa582c369e99b1f220
hannora <hannorayousef@gmail.com>
2026-06-17 03:48:07 +0300

    add a spinner using react-spinner library

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import "./App.css";
 import "./index.css";
 import Header from "./components/Header/Header";
 import Products from "./components/Products/Products";
+import { BounceLoader } from "react-spinners";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [productsData, setProductsData] = useState([]);
   useEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
         const res = await fetch("https://fakestoreapi.com/products");
         const data = await res.json();
+        if (data) setIsLoading(false);
         setProductsData(data);
       } catch (err) {
+        setIsLoading(false);
         console.log(err);
       }
     })();
@@ -20,6 +25,11 @@ const App = () => {
 
   return (
     <>
+      {isLoading && (
+        <div className="loader-container">
+          <BounceLoader color="#9816c5" />
+        </div>
+      )}
       <Header />
       <Products products={productsData} />
     </>
