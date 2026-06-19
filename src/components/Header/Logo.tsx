144aff222a2eb9890a95106a91ffd9cca4266228
hannora <hannorayousef@gmail.com>
2026-06-19 03:21:23 +0300

    make header logo navigate to home page

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom"
+
 const Logo = () => {
   return (
-    <img src='../../public/favicon.svg'/>
+    <Link to="/"><img src='../../public/favicon.svg'/></Link>
   )
 }
 
